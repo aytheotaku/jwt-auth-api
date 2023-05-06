4fd2f5005421a195a0edaa98434199bba20f7849
Ayomide <ayakinbo@gmail.com>
2023-05-06 13:00:30 +0100

    added 'next',allow passage middleware in the chain

--- utils/check_role.js
+++ utils/check_role.js
@@ -2,7 +2,7 @@ const createError = require('http-errors')
 
 const isManager = (req, res, next) => {
     if(!req.user ||  req.user.role !== 'manager' ) return next(createError.Unauthorized('You are not authorized to access this resource'))
-    
+    next()
 }
 
 
