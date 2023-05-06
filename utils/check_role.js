1899904f5c20ce6c954daeabb8fbef21be3dc9f4
Ayomide <ayakinbo@gmail.com>
2023-05-06 22:26:35 +0100

    removed role checks now, an authenticated midware

--- utils/check_role.js
+++ utils/check_role.js
@@ -1,18 +1,10 @@
 const createError = require('http-errors')
 
-const isManager = (req, res, next) => {
-    if(!req.user ||  req.user.role !== 'manager' ) return next(createError.Unauthorized('You are not authorized to access this resource'))
-    next()
-}
-const isReconciler = (req, res, next) => {
-    if(!req.user ||  (req.user.role !== 'reconciler' || req.user.role != 'manager') ) return next(createError.Unauthorized('You are not authorized to access this resource'))
-    next()
-}
-const isRegistrar = (req, res, next) => {
-    if(!req.user ||   (req.user.role !== 'registrar' || req.user.role != 'manager')) return next(createError.Unauthorized('You are not authorized to access this resource'))
+const isAuthenticated = (req, res, next) => {
+    if(!req.user) return next(createError.Unauthorized('You are not authorized to access this resource'))
     next()
 }
 
 
 
-module.exports = { isManager, isReconciler, isRegistrar }
+module.exports = { isAuthenticated }
