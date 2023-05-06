07216a7fbbfc0f7d3a5ff7ff315353f4e48c285a
Ayomide <ayakinbo@gmail.com>
2023-05-06 12:59:31 +0100

    added schema for transaction object

--- utils/obejct_validations.js
+++ utils/obejct_validations.js
@@ -5,7 +5,8 @@ const userCreateSchema = Joi.object({
     lastName : Joi.string().min(1).required().trim(),
     email: Joi.string().email().required().trim(),
     password: Joi.string().min(6).max(12).required(),
-    repeatPassword: Joi.ref('password')
+    repeatPassword: Joi.ref('password'),
+    role: Joi.string().valid('registrar', 'reconciler', 'manager').required().trim()
 })
 
 const userLoginSchema = Joi.object({
@@ -13,6 +14,12 @@ const userLoginSchema = Joi.object({
     password: Joi.string().min(6).max(12).required(),
 })
 
+const transactionCreateSchema = Joi.object({
+    depositorName: Joi.string().lowercase().min(1).required().trim(),
+    transactionAmount : Joi.number().min(1).required(),
+    transactionDate: Joi.date().required()
+})
 
 
-module.exports = { userCreateSchema, userLoginSchema }
+
+module.exports = { userCreateSchema, userLoginSchema, transactionCreateSchema}
