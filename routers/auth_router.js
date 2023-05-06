948d7e483dc27de6eae1f70064ccd5e4584fb1eb
Ayomide <ayakinbo@gmail.com>
2023-05-06 21:59:47 +0100

    added v1 path name. also handlers to RUD

--- routers/auth_router.js
+++ routers/auth_router.js
@@ -6,28 +6,32 @@ const { registerController } = require('../controllers/register_controller');
 const login_controller = require('../controllers/login_controller');
 const protected_route_handler = require('../utils/protected_route_handler');
 const { isManager } = require('../utils/check_role');
-const { get_transactions_controller, create_transaction_controller } = require('../controllers/transactions_controller');
-
+const { get_transactions_controller, create_transaction_controller, delete_transaction_controller, find_transaction_byId_controller, update_transaction_controller } = require('../controllers/transactions_controller');
 
 auth_router.get('/', (req, res) => {
+    res.redirect('/api/v1')
+})
+auth_router.get('/v1', (req, res) => {
     res.send('Api is live').status(200)
 })
 
-auth_router.post('/register', registerController)
+auth_router.post('/v1/register', registerController)
 
-auth_router.post('/login', login_controller)
+auth_router.post('/v1/login', login_controller)
 
-auth_router.get('/protected-route', protected_route_handler, (req, res) => {
+auth_router.get('/v1/protected-route', protected_route_handler, (req, res) => {
     res.status(200).json({
         status: 'Ok',
         message: 'Welcome to the protected route'
     })
 })
 
-auth_router.get('/transactions', protected_route_handler, isManager, get_transactions_controller)
-auth_router.post('/transactions', protected_route_handler, isManager, create_transaction_controller)
+auth_router.get('/v1/transactions', protected_route_handler, isManager, get_transactions_controller)
+auth_router.post('/v1/transactions', protected_route_handler, isManager, create_transaction_controller)
 
-
+auth_router.get('/v1/transactions/:id', protected_route_handler, isManager, find_transaction_byId_controller)
+auth_router.post('/v1/transactions/:id', protected_route_handler, isManager, update_transaction_controller)
+auth_router.delete('/v1/transactions/:id', protected_route_handler, isManager, delete_transaction_controller)
 
 
 
