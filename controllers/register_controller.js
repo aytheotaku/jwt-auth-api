62db83f517ccc24e702173f203abfce15dbad29c
Ayomide <ayakinbo@gmail.com>
2023-05-07 07:04:12 +0100

    fixed userCreateSchema validate async

--- controllers/register_controller.js
+++ controllers/register_controller.js
@@ -9,7 +9,7 @@ const registerController = async (req, res, next) => {
     try {
 
         const {firstName, lastName, email, password, repeatPassword, role} = req.body
-        const result = await userCreateSchema.validateAsync({firstName, lastName, email, password, repeatPassword, role})
+        const result = await userCreateSchema.validateAsync({firstName, lastName, email, password, repeatPassword})
 
         let userExists = await User.findOne({email: result.email})
         if(userExists) return next(createError.Conflict('User Already Exists'))
